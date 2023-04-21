c7303f159a766f14ee95c2439fe9508cdc7a043b
zzd <596006374@qq.com>
2023-04-21 11:36:24 +0800

    added code for input image name

--- codes/cat_fish/main.cpp
+++ codes/cat_fish/main.cpp
@@ -1,17 +1,21 @@
+#include <iostream>
 #include <opencv2/core/core.hpp>
-//#include <opencv2/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-using namespace cv;
-
+//using namespace cv;
+using namespace std;
 int main(int argc, char *argv[])
 {
-    //Mat img = imread("arnold_schwarzenegger.jpg", CV_LOAD_IMAGE_COLOR);
-    Mat img = imread("arnold_schwarzenegger.jpg", IMREAD_COLOR);
+    //cout <<"argc=" << argc << endl << argv[1] << endl << *(argv+1) << endl;;
+    std::string fn;
+    cout << "Input image name for classify:";
+    cin >> fn;
+    cout << fn << endl;
+    cv::Mat img = cv::imread(fn, cv::IMREAD_COLOR);
     if(img.empty())
        return -1;
-    namedWindow("arnold_schwarzenegger", WINDOW_AUTOSIZE );
-    imshow("arnold_schwarzenegger", img);
-    waitKey(0);
+    cv::namedWindow("arnold_schwarzenegger", cv::WINDOW_AUTOSIZE );
+    cv::imshow("arnold_schwarzenegger", img);
+    cv::waitKey(0);
     return 0;
 }
