3a5cc2c5e6c347749f55f9c976d6742bc87821f6
zzd <596006374@qq.com>
2023-04-24 09:36:19 +0800

    finished prediction with onnx

--- codes/cat_fish/main.cpp
+++ codes/cat_fish/main.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/dnn.hpp>
 //using namespace cv;
 using namespace std;
 int main(int argc, char *argv[])
@@ -14,8 +15,44 @@ int main(int argc, char *argv[])
     cv::Mat img = cv::imread(fn, cv::IMREAD_COLOR);
     if(img.empty())
        return -1;
-    cv::namedWindow("arnold_schwarzenegger", cv::WINDOW_AUTOSIZE );
-    cv::imshow("arnold_schwarzenegger", img);
+    cv::namedWindow("picture", cv::WINDOW_AUTOSIZE );
+    cv::imshow("picture", img);
     cv::waitKey(0);
+    cv::destroyWindow("picture");
+
+    //load onnx
+    cv::dnn::Net net = cv::dnn::readNetFromONNX("./onnx/transfer_model.onnx");
+	if(net.empty())
+	{
+		cout << "model loading error!" << endl;
+		system("pause");
+		return -1;
+	}
+	else
+	{
+		cout << "model loading successed!" << endl;
+	}
+
+	cv::Mat inputBlob = cv::dnn::blobFromImage(
+			img,
+			1.0f,
+			cv::Size(64, 64),
+			cv::Scalar(104, 117, 128),
+			true,
+			false);
+	cv::Mat prob;
+	net.setInput(inputBlob);
+	prob = net.forward();
+	cout << "prob mat is " << prob << endl;
+	cv::Mat probMat = prob.reshape(1, 1);  //Mat Mat::reshape(cn, rows=0); if cn==0 channel unchange, if rows == 0 rows unchange.
+	cv::Point classNumber;
+	double classProb;
+	cv::minMaxLoc(probMat, NULL, &classProb, NULL, &classNumber);
+	int label_id = classNumber.x;
+	if(label_id == 0)
+		cout << "cat" << endl;
+	else
+		cout << "fish" << endl;
+
     return 0;
 }
